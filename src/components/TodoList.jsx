b1abadd6d24a21fd766a6551676489b33c6de631
tbackens <t.backens@web.de>
2023-08-26 23:36:29 +0200

    added functions to fetch data

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, addDoc, getDocs, onSnapshot } from "firebase/firestore";
+import {db} from '../firebase';
+
 
 const TodoList = () => {
     const [list, setList] = useState([])
-    const [todo, setTodo] = useState()
-    const [showAddBox, setShowAddBox] = useState(true)
+    const [todo, setTodo] = useState('')
+    const [showAddBox, setShowAddBox] = useState(false)
 
     const handleTextchange = (e) => {
         e.preventDefault();
         setTodo(e.target.value)
     }
 
-    const addTodo = async (data) => {
-        
+    const abortButtonClick = () => {
+        setTodo('');
+        setShowAddBox(false)
     }
+    const addButtonClick = () => {
+        setShowAddBox(true)
+    }
+
+
+
+    const addTodo = async (e) => {
+        e.preventDefault()
+        try {
+            const docRef = await addDoc(collection(db, "todos"), {
+                todo: todo,    
+            });
+            console.log("Document written with ID: ", docRef.id);
+            setTodo('')
+            setShowAddBox(false)    
+        }
+        catch (e) {
+            console.error("Error adding document: ", e);
+          }
+    }
+
+
 
     const addBox = (
         <div>
@@ -22,14 +48,31 @@ const TodoList = () => {
                     <textarea class="form-control" id="todoText" rows="2" value={todo} onChange={handleTextchange}></textarea>
                 </div>
             </form>
-            <button className='btn btn-success' role='submit'>Submit</button>
+            <button className='btn btn-success mx-1' onClick={addTodo}>Submit</button>
+            <button className='btn btn-danger mx-1' onClick={abortButtonClick}>Cancel</button>
         </div>
     )
 
+
+    useEffect(() => {
+        onSnapshot(collection(db, 'todos'), (snapshot) => {
+            setList(snapshot.docs.map(doc => doc.data()))})   
+    }, [])
+
   return (
     <div className="container mt-2">
-        {!showAddBox && <button className='btn btn-success'>Add</button>}
+        {!showAddBox && <button className='btn btn-success' onClick={addButtonClick}>Add</button>}
         {showAddBox && addBox}
+        <div className='mt-4'>
+            <h4>ToDo List</h4>
+            {list.map((i) => {
+                return (<div class="card bg-light m-4" id={i.id}>
+                    <div class="card-body">
+                    {i.todo}
+                    </div>
+              </div>)
+            })}
+        </div>
     </div>
   )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Firebase Configuration
 
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,3 +17,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
