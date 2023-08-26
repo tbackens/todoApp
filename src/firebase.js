7029a7dd5f1aed9a557722b99bcb6a50dcd0ed34
tbackens <t.backens@web.de>
2023-08-26 02:50:00 +0200

    addded firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+// Firebase Configuration
+
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
